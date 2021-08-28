9854e061c7663f156e247744b4d2d78be242ff9e
Alex Shaftel <alexthebassoonguy@gmail.com>
2021-08-28 09:42:34 -0400

    Makes a window, importing set up

--- Classes/Scenario.py
+++ Classes/Scenario.py
@@ -1,18 +1,19 @@
 import pygame
 
 class Scenario:
-    def __init__(self):
+    def __init__(self, name):
         #GLOBAL VARIABLES
         self.CANVAS_SIZE_X = 1000
         self.CANVAS_SIZE_y = 1000
         self.PIXELS_PER_METER = 10
-        self.TICK_SPEED = 10
         self.ROAD_MIN_CURVE_RAD = 10
+        self.FPS = 60
+
+        self.name = name
 
         self.screen = pygame.display.set_mode([self.CANVAS_SIZE_X, self.CANVAS_SIZE_y])
+        pygame.display.set_caption(self.name)
 
-
-    def draw_frame(self, tick):
+    def main(self):
         self.screen.fill((255, 255, 255))
-
         pygame.display.flip()
--- Scenarios/Scenario1_create_car.py
+++ Scenarios/Scenario1_create_car.py
@@ -3,9 +3,14 @@ from Classes.Scenario import Scenario
 pygame.init()
 
 done = False
-main = Scenario()
+main = Scenario("Scenario 1 - Create Car")
+clock = pygame.time.Clock()
 
 while not done:
-    main.draw_frame(1)
+    clock.tick(main.FPS)
+    main.main()
 
-pygame.quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
