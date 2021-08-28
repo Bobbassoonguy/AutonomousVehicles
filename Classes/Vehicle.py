38b9bd6fbac20631f69994bf9ec8905834e754cb
Alex Shaftel <alexthebassoonguy@gmail.com>
2021-08-28 13:11:53 -0400

    made car and wheels display as polygons

--- Classes/Vehicle.py
+++ Classes/Vehicle.py
@@ -1,7 +1,8 @@
 import pygame
+import Shape
 
 class Vehicle:
-    def __init__(self, parent):
+    def __init__(self, parent, x=3, y=4):
         self.parent = parent
         self.surface = self.parent.screen
         self.LENGTH = 4.5  # meters
@@ -10,34 +11,40 @@ class Vehicle:
         self.WHEELBASE = 2.7  # meters
         self.TRACK = 1.5  # meters
         self.MIN_TURN_RADIUS = 10.8  # meters
+        self.PIXELS_PER_METER = self.parent.PIXELS_PER_METER
 
-        self.x = 150
-        self.y = 250
+        self.x = x
+        self.y = y
+
+        self.create_outline()
 
         # TODO fix this because it shouldn't be based on the center of the car
-        self.front_right_wheel = Wheel(self, False,
-                                       (self.x + (self.TRACK * self.parent.PIXELS_PER_METER / 2)),
-                                       (self.y - (self.WHEELBASE * self.parent.PIXELS_PER_METER / 2)))
-        self.front_left_wheel = Wheel(self, False,
-                                      (self.x - (self.TRACK * self.parent.PIXELS_PER_METER / 2)),
-                                      (self.y - (self.WHEELBASE * self.parent.PIXELS_PER_METER / 2)))
-        self.back_right_wheel = Wheel(self, False,
-                                      (self.x + (self.TRACK * self.parent.PIXELS_PER_METER / 2)),
-                                      (self.y + (self.WHEELBASE * self.parent.PIXELS_PER_METER / 2)))
-        self.back_left_wheel = Wheel(self, False,
-                                     (self.x - (self.TRACK * self.parent.PIXELS_PER_METER / 2)),
-                                     (self.y + (self.WHEELBASE * self.parent.PIXELS_PER_METER / 2)))
+        self.front_right_wheel = Wheel(self, True, self.x + (self.TRACK / 2),
+                                       self.y - (self.WHEELBASE / 2))
+        self.front_left_wheel = Wheel(self, True, self.x - (self.TRACK / 2),
+                                       self.y - (self.WHEELBASE / 2))
+        self.back_right_wheel = Wheel(self, True, self.x + (self.TRACK / 2),
+                                       self.y + (self.WHEELBASE / 2))
+        self.back_left_wheel = Wheel(self, True, self.x - (self.TRACK / 2),
+                                       self.y + (self.WHEELBASE / 2))
 
         print("New Car initialized")
 
-    def draw(self):
-        # TODO make this position/rotation dependant
+    def create_outline(self):
         display_border_thickness = 0.05  # meters
         display_rectangle_round_corners = 1.5 * display_border_thickness
+        self.outline = Shape.Shape(self, self.surface,
+                                   [[(self.x - (self.WIDTH/2)) * self.PIXELS_PER_METER, (self.y - (self.LENGTH/2)) * self.PIXELS_PER_METER],
+                                    [(self.x + (self.WIDTH/2)) * self.PIXELS_PER_METER, (self.y - (self.LENGTH/2)) * self.PIXELS_PER_METER],
+                                    [(self.x + (self.WIDTH/2)) * self.PIXELS_PER_METER, (self.y + (self.LENGTH/2)) * self.PIXELS_PER_METER],
+                                    [(self.x - (self.WIDTH/2)) * self.PIXELS_PER_METER, (self.y + (self.LENGTH/2)) * self.PIXELS_PER_METER]],
+                                   (51, 204, 51), line_width=round(display_border_thickness * self.PIXELS_PER_METER),
+                                   fill_color=(153, 255, 153))
 
-        outline = pygame.Rect(self.x-(self.WIDTH * self.parent.PIXELS_PER_METER / 2), self.y-(self.LENGTH * self.parent.PIXELS_PER_METER / 2), self.WIDTH * self.parent.PIXELS_PER_METER, self.LENGTH * self.parent.PIXELS_PER_METER)
-        pygame.draw.rect(self.surface, (153, 255, 153), outline, border_radius=round(display_rectangle_round_corners * self.parent.PIXELS_PER_METER))
-        pygame.draw.rect(self.surface, (51, 204, 51), outline, width=round(display_border_thickness * self.parent.PIXELS_PER_METER), border_radius=round(display_rectangle_round_corners * self.parent.PIXELS_PER_METER))
+    def draw(self):
+        # TODO make this position/rotation dependant
+
+        self.outline.draw()
 
         self.front_right_wheel.draw()
         self.front_left_wheel.draw()
@@ -52,25 +59,32 @@ class Vehicle:
 class Wheel:
     def __init__(self, parent, steering, x, y):
         self.parent = parent
-        self.display_screen = self.parent.surface
+        self.surface = self.parent.surface
         self.WIDTH = 0.18  # meters
         self.DIAMETER = 0.44  # meters
         self.STEERING = False
+        self.PIXELS_PER_METER = self.parent.PIXELS_PER_METER
 
         self.x = x
         self.y = y
 
+        self.create_outline()
+
         # TODO define position and rotation relative to parent vehicle
 
-    def draw(self):
+    def create_outline(self):
         display_border_thickness = 0.02  # meters
-        display_rectangle_round_corners = 1.5 * display_border_thickness
+        self.outline = Shape.Shape(self.parent, self.surface,
+                              [[(self.x - (self.WIDTH / 2)) * self.PIXELS_PER_METER,
+                                (self.y - (self.DIAMETER / 2)) * self.PIXELS_PER_METER],
+                               [(self.x + (self.WIDTH / 2)) * self.PIXELS_PER_METER,
+                                (self.y - (self.DIAMETER / 2)) * self.PIXELS_PER_METER],
+                               [(self.x + (self.WIDTH / 2)) * self.PIXELS_PER_METER,
+                                (self.y + (self.DIAMETER / 2)) * self.PIXELS_PER_METER],
+                               [(self.x - (self.WIDTH / 2)) * self.PIXELS_PER_METER,
+                                (self.y + (self.DIAMETER / 2)) * self.PIXELS_PER_METER]],
+                              (0, 153, 51), line_width=round(display_border_thickness * self.PIXELS_PER_METER),
+                              fill_color=(0, 204, 0))
 
-        outline = pygame.Rect(self.x - (self.WIDTH * self.parent.parent.PIXELS_PER_METER / 2),
-                              self.y - (self.DIAMETER * self.parent.parent.PIXELS_PER_METER / 2),
-                              self.WIDTH * self.parent.parent.PIXELS_PER_METER, self.DIAMETER * self.parent.parent.PIXELS_PER_METER)
-        pygame.draw.rect(self.display_screen, (0, 204, 0), outline,
-                         border_radius=round(display_rectangle_round_corners * self.parent.parent.PIXELS_PER_METER))
-        pygame.draw.rect(self.display_screen, (0, 153, 51), outline,
-                         width=round(display_border_thickness * self.parent.parent.PIXELS_PER_METER),
-                         border_radius=round(display_rectangle_round_corners * self.parent.parent.PIXELS_PER_METER))
+    def draw(self):
+        self.outline.draw()
