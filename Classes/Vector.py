8ce9fff59967aea939f0d23072164acb958a505c
Carson <carsonaveryjones@gmail.com>
2021-08-30 15:12:07 -0400

    vector class with unit vector

--- Classes/Vector.py
+++ Classes/Vector.py
@@ -1,11 +1,14 @@
 import math
 
 class Vector:
-    def __init__(self, x, y):
+    def __init__(self, x, y, unit_vector = False):
         self.x = x
         self.y = y
+        self.unit_vector = unit_vector
 
     def magnitude(self):
+        if self.unit_vector:
+            return 1
         return math.sqrt(self.x**2+self.y**2)
 
     def angle(self):  # returns angle from theta=0 (clockwise from +x axis)
@@ -19,13 +22,16 @@ class Vector:
         self.y = -math.sin(math.radians(degrees))
 
     def scale(self, scale):  # scales vector by its magnitude
-        self.x *= scale
-        self.y *= scale
+        if not self.unit_vector:
+            self.x *= scale
+            self.y *= scale
 
     def set_magnitude(self, magnitude):  # scales a vector such that it points in the same direction and has passed magnitude
-        self.x *= magnitude / math.sqrt(self.x ** 2 + self.y ** 2)
-        self.y *= magnitude / math.sqrt(self.x ** 2 + self.y ** 2)
+        if not self.unit_vector:
+            self.x *= magnitude / math.sqrt(self.x ** 2 + self.y ** 2)
+            self.y *= magnitude / math.sqrt(self.x ** 2 + self.y ** 2)
 
     def ROC_scale(self, ROC_vector, dt): # assuming ROC_vector is in units/dt change self according to passed ROC_vector (i.e.) change self(velocity vector) based on passed vector(acceleration vector) over time period dt(time interval)
-        self.x += ROC_vector[0] * dt
-        self.y += ROC_vector[1] * dt
+        if not self.unit_vector:
+            self.x += ROC_vector[0] * dt
+            self.y += ROC_vector[1] * dt
