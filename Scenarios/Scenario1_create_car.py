3ef4ea82a37d9449c8981b5529c9f04c166cec75
ashaftel <alexthebassoonguy@gmail.com>
2021-09-01 08:24:25 -0400

    Okay I think I fixed git

--- Scenarios/Scenario1_create_car.py
+++ Scenarios/Scenario1_create_car.py
@@ -19,16 +19,17 @@ main.vehicles[0].body.velocity.x = 10
 main.vehicles[0].body.acceleration.x = 10
 main.vehicles[0].current_turn_radius = 10
 
-# main.addCar(x=20, y=45, fill_color=Colors.ORANGE_FILL, outline_color=Colors.ORANGE_LINE)
-# main.vehicles[1].rotate(180, main.vehicles[1].body.position())
-# main.vehicles[1].body.velocity.x = -40
-# main.vehicles[1].current_turn_radius = -7
+main.addCar(x=20, y=45, fill_color=Colors.ORANGE_FILL, outline_color=Colors.ORANGE_LINE)
+main.vehicles[1].rotate(180, main.vehicles[1].body.position())
+main.vehicles[1].body.velocity.x = -40
+main.vehicles[1].current_turn_radius = -7
 
 while not done:
     clock.tick(main.globals.FPS)
     main.main()
 
     main.vehicles[0].go(1/main.globals.FPS)
+    main.vehicles[1].go(1 / main.globals.FPS)
 
     pygame.display.flip()
 
