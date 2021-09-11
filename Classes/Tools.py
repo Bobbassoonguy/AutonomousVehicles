dc681834448a1d875530ac50f524e1acf22562b7
Alex Shaftel <alexthebassoonguy@gmail.com>
2021-09-11 14:48:36 -0400

    started a Tools.py file for useful functions

---
+++ Classes/Tools.py
@@ -0,0 +1,18 @@
+import math
+import pygame
+import Vector
+import Colors
+
+
+def draw_arc(globals, surface, start_point, end_point, radius, width, color):
+    print("Placeholder")
+
+
+def get_arc_points(globals, start_point, end_point, radius, width):
+    # Note: if radius is positive, the turn is a "right turn" from the start_point
+    # Note: all values are in meters, but it shouldnt matter mathematically.
+    # Note: round numbers to 5 decimal places?
+    points = []
+    print("Placeholder")
+
+    return points
