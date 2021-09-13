4027ef8df0464b47d23b0693d6a341207d9992c3
ashaftel <alexthebassoonguy@gmail.com>
2021-09-13 11:05:50 -0400

    arc point gen is now based on center and start and end angles. Tools.two_point_arc_circle can determine these parameters.

--- Classes/Tools.py
+++ Classes/Tools.py
@@ -40,7 +40,9 @@ def draw_double_line(globals, surface, start_point, end_point, width, color): # 
 
 
 def draw_arc(globals, surface, start_point, end_point, radius, width, color, inside=True):
-    points = get_arc_points(globals, start_point, end_point, radius, width, inside)
+    #TODO make it so that if the passed width is ~1 pixel use the builtin arc drawing function
+    center, start_angle, end_angle = two_point_arc_circle(start_point,end_point,radius)
+    points = get_arc_outline_points(globals,center,abs(radius),start_angle,end_angle,width,inside)
     pixel_points = []
     for i in points:
         pixel_points.append(globals.point_to_pixels(i))
@@ -51,55 +53,47 @@ def draw_arc(globals, surface, start_point, end_point, radius, width, color, ins
     # pygame.draw.circle(surface, Colors.CAR_RED_LINE, globals.point_to_pixels(end_point), 7, width=1)
 
 
-
-def get_arc_points(globals, start_point, end_point, radius, width, inside=True):
+def two_point_arc_circle(start_point, end_point, radius):
+    # returns the center point and angle swept (radians) of an arc from start_point to end_point with given radius
     # Note: if radius is positive, the turn is a clockwise turn from the start_point
-    # Note: all values are in meters, but it shouldnt matter mathematically.
-    # Note: round numbers to 5 decimal places?
-    f0 = 20/(globals.PIXELS_PER_METER)  # 0.25 meters
-    # f0 = 2
-    points_vector1 = Vector.get_Vector_between_points(start_point, end_point)
-    points_vector2 = Vector.get_Vector_between_points(start_point, end_point)
-
+    points_vector = Vector.get_Vector_between_points(start_point, end_point)
     center_point = [(start_point[0] + end_point[0]) / 2, (start_point[1] + end_point[1]) / 2]
     dist_to_center_point = math.sqrt(
         ((end_point[0] - start_point[0]) ** 2) + ((end_point[1] - start_point[1]) ** 2)) / 2
     if abs(radius) < dist_to_center_point:
         raise ValueError('Tried to generate an arc with a radius smaller than 1/2 the distance between the points.')
-    points_vector1.rotate(90)
-    points_vector2.rotate(-90)
 
-    points_vector1.set_magnitude(math.sqrt((radius ** 2) - (dist_to_center_point ** 2)))
-    points_vector2.set_magnitude(math.sqrt((radius ** 2) - (dist_to_center_point ** 2)))
-
-    points_vector1 += center_point
-    points_vector2 += center_point
-
-    center_1 = points_vector1.list()
-    center_1_to_start_v = Vector.get_Vector_between_points(center_1, start_point)
-    center_1_to_end_v = Vector.get_Vector_between_points(center_1, end_point)
-    center_1_to_start = math.radians(center_1_to_start_v.angle())
-    center_1_to_end = math.radians(center_1_to_end_v.angle())
-
-    center_2 = points_vector2.list()
-    center_2_to_start_v = Vector.get_Vector_between_points(center_2, start_point)
-    center_2_to_end_v = Vector.get_Vector_between_points(center_2, end_point)
-    center_2_to_start = math.radians(center_2_to_start_v.angle())
-    center_2_to_end = math.radians(center_2_to_end_v.angle())
-
-    center_to_end = center_2_to_end
-    center_to_start = center_2_to_start
-    center_to_start_v = center_2_to_start_v
-    center_to_end_v = center_2_to_end_v
-    center = center_2
     if radius < 0:
-        center_to_end = center_1_to_end
-        center_to_start = center_1_to_start
-        center_to_start_v = center_1_to_start_v
-        center_to_end_v = center_1_to_end_v
-        center = center_1
+        points_vector.rotate(90)
+    else:
+        points_vector.rotate(-90)
 
-    phi = (center_to_end-center_to_start) % (2 * math.pi)
+    points_vector.set_magnitude(math.sqrt((radius ** 2) - (dist_to_center_point ** 2)))
+
+    points_vector += center_point
+
+    center = points_vector.list()
+    center_to_start_v = Vector.get_Vector_between_points(center, start_point)
+    center_to_end_v = Vector.get_Vector_between_points(center, end_point)
+    center_to_start = math.radians(center_to_start_v.angle())
+    center_to_end = math.radians(center_to_end_v.angle())
+
+    print("Center: ", center)
+    print("Center to start: ", math.degrees(center_to_start))
+    print("Center to end: ", math.degrees(center_to_end))
+
+    return center, center_to_start, center_to_end
+
+
+def get_arc_outline_points(globals, center, radius, start_angle, end_angle, width, inside=True):
+    # Note: if radius is positive, the turn is a clockwise turn from the start_point
+    # Note: all values are in meters, but it shouldnt matter mathematically.
+    # Note: angles are in RADIANS
+    # Note: round numbers to 5 decimal places?
+    f0 = 20/(globals.PIXELS_PER_METER)  # 0.25 meters
+    # f0 = 2
+
+    phi = (end_angle-start_angle) % (2 * math.pi)
     if phi > math.pi:
         phi -= 2*math.pi
     theta_0 = 2*math.asin(f0/(2*radius))
@@ -107,13 +101,11 @@ def get_arc_points(globals, start_point, end_point, radius, width, inside=True):
     theta = phi/n
     f = 2*radius*math.sin(theta/2)
 
-    # print("center to start v: ",center_to_start_v.list())
-    # print("center: ", center)
-    # print("Starting angle: ", center_to_start_v.angle())
-    # print("Ending angle: ", center_to_end_v.angle())
-    # print("N: ", n)
-    # print("phi: ", math.degrees(phi))
-    # print("theta: ", math.degrees(theta))
+    center_to_start_v = Vector.Vector(radius, 0)
+    center_to_start_v.rotate_to(math.degrees(start_angle))
+    center_to_end_v = Vector.Vector(radius, 0)
+    center_to_end_v.rotate_to(math.degrees(end_angle))
+
     points = []
     points.append(center_to_start_v)
     current_point = center_to_start_v.copy()
--- Scenarios/Scenario1_create_car.py
+++ Scenarios/Scenario1_create_car.py
@@ -16,15 +16,20 @@ clock = pygame.time.Clock()
 
 main.globals.FPS = 30
 
-main.road_map.add_road([[30,20],[150,10,-100],[130,150],[120,80],[25,30]])
+# main.road_map.add_road([[30,20],[150,10,-100],[130,150],[120,80],[25,30]])
 #main.road_map.add_road([[10, 45], [35, 10]])
 main.road_map.draw_roads()
 main.road_map.draw_lines(road_segment_points=True)
 # main.globals.draw_arc(main.globals.VEHICLE_SURFACE, Colors.CAR_PURPLE_FILL, [30, 30], [80, 60], 30, 10)
 
-# Tools.draw_arc(main.globals,main.globals.VEHICLE_SURFACE,[60,60],[120,140],-150,2*3.66,Colors.ROAD_ORANGE)
-Tools.draw_dashed_line(main.globals, main.globals.VEHICLE_SURFACE,[60,100],[100,100],0.15,Colors.MAP_CYAN)
-Tools.draw_double_line(main.globals, main.globals.VEHICLE_SURFACE,[60,120],[100,150],0.15,Colors.MAP_YELLOW)
+# Tools.draw_dashed_line(main.globals, main.globals.VEHICLE_SURFACE,[60,100],[100,100],0.15,Colors.MAP_CYAN)
+# Tools.draw_double_line(main.globals, main.globals.VEHICLE_SURFACE,[60,120],[100,150],0.15,Colors.MAP_YELLOW)
+
+test_start = [100,100]
+test_end = [120,120]
+test_rad = 40
+Tools.draw_arc(main.globals,main.globals.VEHICLE_SURFACE,test_start,test_end,test_rad,1*3.66,Colors.ROAD_ORANGE)
+
 
 while not done:
     time_delta = clock.tick(main.globals.FPS) / 1000
